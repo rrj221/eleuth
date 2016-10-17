e143520f370b074686d336c0cd55180d1f384dfe
jrizza88 <jetorres88@gmail.com>
2016-10-17 13:26:51 -0400

    created registration, form and login components

--- src/client/Components/LoginPage.js
+++ src/client/Components/LoginPage.js
@@ -1,14 +1,48 @@
 import React, {Component} from 'react';
 
 import styled from 'styled-components';
+import Form from './Form';
+
+import {loginRequest} from '../actions';
 
 export default class LoginPage extends Component {
   render() {
+    constructor (props) {
+    super(props)
+
+    this._login = this._login.bind(this)
+  }
+
+  render () {
+    let {dispatch} = this.props
+    let {formState, currentlySending, error} = this.props.data
+
     return (
-      <div id='LoginPage'>
-        LoginPage
+      <div className='form-page__wrapper'>
+        <div className='form-page__form-wrapper'>
+          <div className='form-page__form-header'>
+            <h2 className='form-page__form-heading'>Login</h2>
+          </div>
+          <Form data={formState} dispatch={dispatch} history={this.props.history} onSubmit={this._login} btnText={'Login'} error={error} currentlySending={currentlySending} />
+        </div>
       </div>
     )
+  }
+
+  _login (username, password) {
+    this.props.dispatch(loginRequest({username, password}))
+  }
+}
+
+Login.propTypes = {
+  data: React.PropTypes.object,
+  history: React.PropTypes.object,
+  dispatch: React.PropTypes.func
+}
+
+function select (state) {
+  return {
+    data: state
   }
 }
 
