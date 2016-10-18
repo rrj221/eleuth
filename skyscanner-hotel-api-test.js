7675bd7d7d7b586bcdba56f6c3dbf0a9f1c3a0ff
Andrew Frenkel <Andrew Frenkel>
2016-10-18 19:18:41 -0400

    oct 18

--- skyscanner-hotel-api-test.js
+++ skyscanner-hotel-api-test.js
@@ -10,22 +10,35 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
-var destination = getElementById('hotel-destination').value || 'tokyo', 
+var destination = 
+// getElementById('hotel-destination').value || 
+'tokyo', 
 apikey = 'prtl6749387986743898559646983194', 
-checkin = getElementById('checkin').value || '', 
-checkout = getElementById('checkout').value || '', 
-guests = getElementById('guests').value || '1', 
-rooms = getElementById('rooms').value || '1'; 
+checkin = 
+// document.getElementById('checkin').value || 
+'2016-10-15T19:30:00', 
+checkout = 
+// document.getElementById('checkout').value || 
+'2016-10-15T20:55:00', 
+guests = 
+// document.getElementById('guests').value || 
+'1', 
+rooms = 
+// document.getElementById('rooms').value || 
+'1'; 
 
-router.get('/sky', function(req, res) {
+app.get('/hotel', function(req, res) {
 	unirest.get('http://partners.api.skyscanner.net/apiservices/hotels/autosuggest/v2/US/USD/en-us/' + destination + '?apikey=' + apikey)
 		.headers({
-			'Content-Type': 'application/x-www-form-urlencoded',
 			Accept: 'application/json'
-		});
-	console.log(res);
-});
+		}).end(function(data){
+	res.json(data);})
+	})
 
 
 // router.get('/sky', function(req, res){
 // 	unirest.get('http://partners.api.skyscanner.net/apiservices/hotels/liveprices/v2/US/USD/en-us/{entityid}/{checkindate}/{checkoutdate}/{guests}/{rooms}?apiKey=' + apikey);
+
+app.listen(PORT, function(){
+	console.log('listening on', PORT);
+});
